0a4ca018effb0d9df42c6b499886ea4e6aff761f
catalinmiron <mironcatalin@gmail.com>
2015-06-30 23:32:37 +0300

    Refactor shot details #2
    Add comments #2

--- app/ShotDetails.js
+++ app/ShotDetails.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var React = require('react-native');
+var React = require("react-native");
 var {
   Image,
   PixelRatio,
@@ -9,24 +9,34 @@ var {
   Text,
   TouchableOpacity,
   TouchableHighlight,
+  ActivityIndicatorIOS,
   View,
+  ListView,
   Component
 } = React;
 
-var Icon = require('FontAwesome'),
-    getImage = require('./helpers/getImage'),
-    HTML = require('react-native-htmlview'),
-    screen = require('Dimensions').get('window'),
-    ParallaxView = require('react-native-parallax-view'),
-    Modal = require('react-native-modal');
+var api = require("./helpers/api");
 
-var Player = require('./Player');
+var Icon = require("FontAwesome"),
+    getImage = require("./helpers/getImage"),
+    HTML = require("react-native-htmlview"),
+    screen = require("Dimensions").get("window"),
+    ParallaxView = require("react-native-parallax-view"),
+    Modal = require("react-native-modal");
+
+var Player = require("./Player");
+var CommentItem = require("./CommentItem");
+var Loading = require("./Loading");
 
 var ShotDetails = React.createClass({
   getInitialState: function() {
     return {
-      isModalOpen: false
-    }
+      isModalOpen: false,
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+    };
   },
 
   openModal: function() {
@@ -41,8 +51,17 @@ var ShotDetails = React.createClass({
     });
   },
 
+  componentDidMount: function() {
+    api.getResources(this.props.shot.comments_url).then((responseData) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(responseData),
+        isLoading: false
+      });
+    }).done();
+  },
+
   render: function() {
-    var player = this.props.shot.player;
+    var player = this.props.shot.user;
 
     return (
       <ParallaxView
@@ -56,7 +75,9 @@ var ShotDetails = React.createClass({
         >
         <View>
           <TouchableHighlight style={styles.invisibleTouch}
-                              onPress={this.selectPlayer.bind(this, player)}>
+                              onPress={this.selectPlayer.bind(this, player)}
+                              underlayColor={"#333"}
+                              activeOpacity={0.95}>
             <View style={styles.headerContent}>
               <Image source={getImage.authorAvatar(player)}
                      style={styles.playerAvatar} />
@@ -84,6 +105,11 @@ var ShotDetails = React.createClass({
               <HTML value={this.props.shot.description}
                     stylesheet={styles}/>
             </Text>
+            <View>
+              {this.state.dataSource.getRowCount() === 0 ?
+                <Loading /> :
+                this._renderCommentsList()}
+            </View>
           </View>
         </View>
         <Modal isVisible={this.state.isModalOpen}
@@ -103,12 +129,12 @@ var ShotDetails = React.createClass({
   },
 
   _showModalTransition: function(transition) {
-    transition('opacity', {
+    transition("opacity", {
       duration: 200,
       begin: 0,
       end: 1
     });
-    transition('height', {
+    transition("height", {
       duration: 200,
       begin: - screen.height * 2,
       end: screen.height
@@ -116,13 +142,13 @@ var ShotDetails = React.createClass({
   },
 
   _hideModalTransition: function(transition) {
-    transition('height', {
+    transition("height", {
       duration: 200,
       begin: screen.height,
       end: screen.height * 2,
       reset: true
     });
-    transition('opacity', {
+    transition("opacity", {
       duration: 200,
       begin: 1,
       end: 0
@@ -136,16 +162,50 @@ var ShotDetails = React.createClass({
       title: player.name
     });
   },
+
+  _renderCommentsList: function() {
+    return <View style={styles.sectionSpacing}>
+      <View style={styles.separator} />
+      <Text style={styles.heading}>Comments</Text>
+      <View style={styles.separator} />
+      <ListView
+        ref="commentsView"
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+        automaticallyAdjustContentInsets={false}
+      />
+    </View>
+  },
+
+  renderRow: function(comment: Object) {
+    return <CommentItem
+      onSelect={() => this.selectPlayer(comment.user)}
+      comment={comment} />;
+  },
+
+  _renderLoading: function() {
+    return <ActivityIndicatorIOS animating={this.state.isLoading}
+                                 style={styles.spinner}/>;
+  }
 });
 
 var styles = StyleSheet.create({
+  spinner: {
+    marginTop: 20,
+    width: 50
+  },
   a: {
-    fontWeight: '300',
-    color: '#ea4c89'
+    fontWeight: "300",
+    color: "#ea4c89"
+  },
+  p: {
+    marginBottom: 0,
+    flexDirection: "row",
+    marginTop: 0,
   },
   invisibleView: {
     flex: 1,
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
@@ -158,63 +218,70 @@ var styles = StyleSheet.create({
     flex: 1,
     paddingBottom: 20,
     paddingTop: 40,
-    alignItems: 'center',
+    alignItems: "center",
     width: screen.width,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   },
   shotTitle: {
     fontSize: 16,
-    fontWeight: '400',
-    color: '#ea4c89',
+    fontWeight: "400",
+    color: "#ea4c89",
     lineHeight: 18
   },
   playerContent: {
     fontSize: 12
   },
   player: {
-    fontWeight: '900',
+    fontWeight: "900",
     lineHeight: 18
   },
   playerAvatar: {
     borderRadius: 40,
     width: 80,
     height: 80,
-    position: 'absolute',
+    position: "absolute",
     bottom: 60,
     left: screen.width / 2 - 40,
     borderWidth: 2,
-    borderColor: '#fff'
+    borderColor: "#fff"
   },
   rightPane: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center'
+    flexDirection: "column",
+    alignItems: "center"
   },
   shotDetailsRow: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    flexDirection: 'row'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    flexDirection: "row"
   },
   shotCounter: {
     flex: 2,
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   shotCounterText: {
-    color: '#333'
+    color: "#333"
   },
   mainSection: {
-    backgroundColor: 'white',
     flex: 1,
-    alignItems: 'stretch',
-    padding: 10
+    alignItems: "stretch",
+    padding: 10,
+    backgroundColor: "white"
   },
   separator: {
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
     height: 1 / PixelRatio.get(),
     marginVertical: 10,
+  },
+  sectionSpacing: {
+    marginTop: 20
+  },
+  heading: {
+    fontWeight: "700",
+    fontSize: 16
   }
 });
 
