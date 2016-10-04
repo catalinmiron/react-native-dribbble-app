97e804f582ce3209bb4c68253fb57086b4611d04
Sharper <sharp.bruce.lee@gmail.com>
2016-10-04 14:06:34 +0800

    upgrade to es6

--- app/helpers/getImage.js
+++ app/helpers/getImage.js
@@ -1,18 +1,18 @@
-'use strict';
+//@flow
 
-module.exports = {
-  shotImage: function(shot: Object): {uri: ?string} {
+export function shotImage (shot: Object): {uri: ?string} {
     var uri = shot.images.normal ? shot.images.normal : shot.images.teaser;
     return {uri};
-  },
-  authorAvatar: function(player: Object): {uri: ?string} {
-    var uri;
-    if (player) {
-      uri = player.avatar_url;
-      return {uri};
-    } else {
-      uri = require('../../img/AuthorAvatar.png');
-      return uri;
-    }
-  }
 }
+
+export function authorAvatar(player: Object): {uri: ?string} {
+    var uri;
+
+    if (player) {
+        uri = player.avatar_url;
+        return {uri};
+    } else {
+        uri = require('../../img/AuthorAvatar.png');
+        return uri;
+    }
+ }
