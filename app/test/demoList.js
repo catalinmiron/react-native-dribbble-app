6ce8422c09f3ad885915597087d2568ea83f1bd9
Sharper <sharp.bruce.lee@gmail.com>
2016-10-07 01:42:51 +0800

    ES6

--- app/test/demoList.js
+++ app/test/demoList.js
@@ -7,7 +7,8 @@ import {
   TouchableHighlight,
   ListView,
   StyleSheet,
-  ActivityIndicator
+  ActivityIndicator,
+  RefreshControl,
 } from 'react-native';
 
 import DemoCell from './demoCell';
@@ -21,8 +22,8 @@ export default class DemoList extends React.Component {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      isLoading: false,
       isLoadingTail: false,
+      isRefreshing: false,
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
       }),
@@ -43,10 +44,14 @@ export default class DemoList extends React.Component {
     this._renderSeparator = this._renderSeparator.bind(this);
     this._pressRow = this._pressRow.bind(this);
     this._renderView = this._renderView.bind(this);
+    this._renderFooter = this._renderFooter.bind(this);
 
     // bind ds
     this._getDataSource = this._getDataSource.bind(this);
     this._getShots = this._getShots.bind(this);
+    this._onRefresh = this._onRefresh.bind(this);
+    this._hasMore = this._hasMore.bind(this);
+    this._onEndReached = this._onEndReached.bind(this);
   }
 
   componentWillMount() {
@@ -55,7 +60,8 @@ export default class DemoList extends React.Component {
 
   _getShots(query: string) {
     this.setState({
-      isLoading: true,
+      // isLoading: true,
+      isRefreshing: true,
       queryNumber: this.state.queryNumber + 1,
       isLoadingTail: false,
     });
@@ -66,11 +72,17 @@ export default class DemoList extends React.Component {
 
       this.setState({
           dataSource: this._getDataSource([]),
-          isLoading: false,
+          // isLoading: false,
+          isRefreshing: false
       });
     }).then((responseData) => {
+      this.LOADING[query] = false;
+      this.resultsCache.dataForQuery[query] = responseData;
+      this.resultsCache.nextPageNumberForQuery[query] = 2;
+
       this.setState({
-        isLoading: false,
+        // isLoading: false,
+        isRefreshing: false,
         dataSource: this._getDataSource(responseData),
       });
     }).done();
@@ -78,6 +90,82 @@ export default class DemoList extends React.Component {
 
   _getDataSource(shots: Array<any>): ListView.DataSource {
     return this.state.dataSource.cloneWithRows(shots);
+  }
+
+  _onRefresh() {
+    this.setState({isRefreshing: true});
+    this._getShots(this.state.filter);
+  }
+
+  _hasMore(): boolean {
+    var query = this.state.filter;
+    if (!this.resultsCache.dataForQuery[query]) {
+        return true;
+    }
+    return (
+        this.resultsCache.totalForQuery[query] !==
+          this.resultsCache.dataForQuery[query].length
+    );
+  }
+
+  _onEndReached() {
+    var query = this.state.filter;
+    if (!this._hasMore() || this.state.isLoadingTail || this.state.isRefreshing) {
+        // We"re already fetching or have all the elements so noop
+        return;
+    }
+
+    if (this.LOADING[query]) {
+        return;
+    }
+
+    this.LOADING[query] = true;
+    this.setState({
+        queryNumber: this.state.queryNumber + 1,
+        isLoadingTail: true,
+    });
+
+    let page = this.resultsCache.nextPageNumberForQuery[query];
+    let resultsCache = this.resultsCache;
+
+    api.getShotsByType(query, page)
+      .catch((error) => {
+        this.LOADING[query] = false;
+        this.setState({
+            isLoadingTail: false,
+        });
+      })
+      .then((responseData) => {
+        // if(!resultsCache.dataForQuery[query]) {
+        //   console.warn('result is invalid');
+        //   this.setState({
+        //       isLoadingTail: false,
+        //       // dataSource: this.getDataSource(this.resultsCache.dataForQuery[query]),
+        //   });
+        //
+        //   return;
+        // }
+
+        let shotsForQuery = resultsCache.dataForQuery[query].slice();
+
+        this.LOADING[query] = false;
+        // We reached the end of the list before the expected number of results
+        if (!responseData) {
+            resultsCache.totalForQuery[query] = shotsForQuery.length;
+        } else {
+            for (var i in responseData) {
+                shotsForQuery.push(responseData[i]);
+            }
+            resultsCache.dataForQuery[query] = shotsForQuery;
+            resultsCache.nextPageNumberForQuery[query] += 1;
+        }
+
+        this.setState({
+            isLoadingTail: false,
+            dataSource: this._getDataSource(resultsCache.dataForQuery[query]),
+        });
+      })
+      .done();
   }
 
   _renderRow(rowData: Object, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
@@ -106,19 +194,33 @@ export default class DemoList extends React.Component {
     });
   }
 
+  _renderFooter() {
+      return <View style={styles.scrollSpinner}>
+          <UNActivityIndicator loadingType={LOADING_TYPE.Default} />
+      </View>;
+  }
+
   _renderView() {
     if (this.state.isLoading) {
       return (
-        <UNActivityIndicator loadingType={LOADING_TYPE.Large} />
+        <Text>Empty Data</Text>
       );
     }
 
     return (
       <View style={styles.container}>
         <ListView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this._onRefresh}
+            />
+          }
           dataSource={this.state.dataSource}
           renderRow={this._renderRow}
           renderSeparator={this._renderSeparator}
+          renderFooter={this._renderFooter}
+          onEndReached={this._onEndReached}
           automaticallyAdjustContentInsets={false}
           enableEmptySectoins={false}
           />
@@ -161,6 +263,9 @@ const styles = StyleSheet.create({
   seperator: {
     height: 1,
     backgroundColor: '#eeeeee'
+  },
+  scrollSpinner: {
+    marginVertical: 20,
   },
 
 });
