8857180577d87b77aa76a9f196ef7d85cf5ab66c
future-challenger <sunshine_anycall@hotmail.com>
2016-11-20 12:40:12 +0800

    redux

--- app/store/configStore.js
+++ app/store/configStore.js
@@ -4,11 +4,29 @@
   @flow
 */
 import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk'
+import createLogger fom 'redux-logger'
+// import { persistStore, autoRehydrate } from 'redux-persist'
+// import { AsyncStorage } from 'react-native'
+import reducers from './reducers'
 
+var logger = createLogger({
+  predicate: (getState, action) => isDebuggingInChrome,
+  callapsed: true,
+  duration: true,
+})
+
+var createAppStore = applyMiddleware(thunk, logger)(createStore)
 
 
 var isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent
 
 export default function configureStore(onComplete: ?() => void) {
-  // const store = autoRe
+  // const store = autoRehydrate()(createAppStore)(reducers)
+  const store = createAppStore(reducers)
+  if (isDebuggingInChrome) {
+    window.store = store
+  }
+
+  return store 
 }
